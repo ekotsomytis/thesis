f1be28b1360715cd33a127c293abc10400b8687c
bagelhs kotsomytis <v.kotsomitis@taxaki.com>
2024-04-22 22:56:13 +0300

    change stack

---
+++ java/src/main/java/com/example/thesisprototype/ThesisPrototypeApplication.java
@@ -0,0 +1,13 @@
+package com.example.thesisprototype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThesisPrototypeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThesisPrototypeApplication.class, args);
+	}
+
+}
---
+++ java/src/test/java/com/example/thesisprototype/ThesisPrototypeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thesisprototype;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThesisPrototypeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
